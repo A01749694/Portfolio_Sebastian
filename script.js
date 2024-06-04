e5d2ffd11f872b57997a2cb5d54efa55ae31034a
Seb <117104224+A01749694@users.noreply.github.com>
2024-06-03 19:56:46 -0600

    Version 1.0

---
+++ script.js
@@ -0,0 +1,11 @@
+function navigateTo(url) {
+    window.location.href = url;
+}
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    }
+
+    function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    }
